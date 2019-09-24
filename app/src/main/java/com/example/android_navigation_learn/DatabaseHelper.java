d9fd3f9f4668a3f02577131c95dacbb139946bc6
domingowen <domingowen645@gmail.com>
2019-09-24 08:59:55 +0800

    database learn

---
+++ app/src/main/java/com/example/android_navigation_learn/DatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.android_navigation_learn;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Finance.db";
+
+    public static final String TABLE_NAME = "Finance_tab";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "DATA";
+    public static final String COL_4 = "STATEMENT";
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table" + TABLE_NAME + COL_1 + COL_2);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public Cursor getAllData() {
+
+
+
+    }
+}
